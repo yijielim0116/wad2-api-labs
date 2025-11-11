c38b217a5799e5cfcee6c28eb40dd90fdc874b18
Yi Jie Lim <yijielim95@Yi-Jies-MacBook-Air.local>
2025-11-11 12:16:24 +0000

    Express Lab - exercise done

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -22,13 +22,16 @@ router.get('/:id', (req, res) => {
 //Add a task
 router.post('/', (req, res) => {
     const { title, description, deadline, priority, done } = req.body;
+    const now = new Date().toISOString();
     const newTask = {
         id: uuidv4(),
         title,
         description,
         deadline,
         priority,
-        done
+        done,
+        created_at: now,
+        updated_at: now
     };
     tasksData.tasks.push(newTask);
     res.status(201).json(newTask);
@@ -42,7 +45,9 @@ router.put('/:id', (req, res) => {
     if (taskIndex === -1) {
         return res.status(404).json({ status: 404, message: 'Task not found' });
     }
-    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id:id };
+    const existingTask = tasksData.tasks[taskIndex];
+    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id:id ,created_at: existingTask.created_at, 
+        updated_at: new Date().toISOString()};
     tasksData.tasks[taskIndex] = updatedTask;
     res.json(updatedTask);
 });
