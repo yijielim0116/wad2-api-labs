e24a2a6857e6d39f9efb6dd1adabb301aa5e67dd
Yi Jie Lim <yijielim95@Yi-Jies-MacBook-Air.local>
2025-11-25 12:34:59 +0000

    Added userId to tasks

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -6,7 +6,13 @@ const router = express.Router();
 
 // Get all tasks
 router.get('/', async (req, res) => {
-    const tasks = await Task.find();
+    const tasks = await Task.find().populate('userId', 'username');
+    res.status(200).json(tasks);
+});
+
+// Get a user's tasks
+router.get('/user/:uid', async (req, res) => {
+    const tasks = await Task.find({ userId: `${req.params.uid}`});
     res.status(200).json(tasks);
 });
 
